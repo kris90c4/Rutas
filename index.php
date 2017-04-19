8557fa8881a56a858dce61a5a9438d57c84f84ae
Cristian <cristiansmx2a@gmail.com>
2017-04-19 23:39:24 +0200

    Añadido menu provisional con css externo

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
+<?php 
+	define("__RAIZ__","C:\Users\Bender\Dropbox\FP Superior\2 Curso\Aplicaciones\Rutas");
+	
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Rutas</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="asset/css/style.css">
     </head>
     <body>
-        <nav><span>hola</span></nav>
+        <nav>
+	        <ul>
+	        	<li><a href="index.php">Inicio</a></li>
+	        	<li class="right"><a href="login.php">login</a></li>
+	        	<li class="right"><a href="registrar.php">registrar</a></li>
+	        </ul>
+        </nav>
     </body>
 </html>
 
