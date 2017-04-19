1a24be4053df644e95cc12e8bedc3bc8dae7591a
Cristian <cristiansmx2a@gmail.com>
2017-04-19 23:48:11 +0200

    Se crea un modal de prueba

--- index.php
+++ index.php
@@ -14,10 +14,50 @@
         <nav>
 	        <ul>
 	        	<li><a href="index.php">Inicio</a></li>
-	        	<li class="right"><a href="login.php">login</a></li>
+	        	<li class="right"><a id="myBtn">login</a></li>
 	        	<li class="right"><a href="registrar.php">registrar</a></li>
 	        </ul>
         </nav>
+ 
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+        
     </body>
 </html>
 
