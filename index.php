8222b50bce12e38744019aab193f2b3e1a7d2ad2
Cristian <cristiansmx2a@gmail.com>
2017-05-08 00:03:09 +0200

    bd

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Rutas</title>
+		<title>Gestoria</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="asset/css/style.css">
