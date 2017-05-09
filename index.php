730fddd00e36cb05706495f0033f45278a5c7f81
Cristian <cristiansmx2a@gmail.com>
2017-05-09 22:58:56 +0200

    Login

--- index.php
+++ index.php
@@ -12,35 +12,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="asset/css/style.css">
-		<script>
-			window.onload=function(){
-				// Get the modal
-				var modal = document.getElementById('myModal');
-				
-				// Get the button that opens the modal
-				var btn = document.getElementById("myBtn");
-				
-				// Get the <span> element that closes the modal
-				var span = document.getElementsByClassName("close")[0];
-				
-				// When the user clicks the button, open the modal 
-				btn.onclick = function() {
-					modal.style.display = "block";
-				}
-				
-				// When the user clicks on <span> (x), close the modal
-				span.onclick = function() {
-					modal.style.display = "none";
-				}
-				
-				// When the user clicks anywhere outside of the modal, close it
-				window.onclick = function(event) {
-					if (event.target == modal) {
-						modal.style.display = "none";
-					}
-				}
-			}
-		</script>
+		<script type="text/javascript" src="asset/js/index.js"></script>
 	</head>
 	<body>
 		<nav>
@@ -57,7 +29,13 @@
 		  <!-- Modal content -->
 			<div class="modal-content">
 				<span class="close">&times;</span>
-				<p>Some text in the Modal..</p>
+				<form id="login" method="POST">
+				<label>Usuario</label>
+				<input type="text" name="login">
+				<label>Contraseña</label>
+				<input type="password" name="contrasena"><br>
+				<input type="submit" value="Iniciar Sesion">
+				</form>
 			</div>
 		
 		</div>
