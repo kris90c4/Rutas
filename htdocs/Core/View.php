58539107d5cd58e16b8c3e1732e20041b18bfbf0
Cristian <cristiansmx2a@gmail.com>
2017-05-15 23:52:36 +0200

    Comentada clase core/view

--- htdocs/Core/View.php
+++ htdocs/Core/View.php
@@ -30,13 +30,21 @@ class View
 		{
 			throw new \Exception("Error: El archivo " . self::VIEWS_PATH . $template . "." . self::EXTENSION_TEMPLATES . " no existe", 1);
 		}
+		//Cargo el menu en la variable $menu
 		$menu=file_get_contents(self::VIEWS_PATH . "menu." . self::EXTENSION_TEMPLATES);
+		//Se comienza a almacenar en el buffer los datos que salgan por pantalla
 		ob_start();
+		//Se extraen todos los indices y se convierten en variables
 		extract(self::$data);
+		//Se incluye la vista que el controlador manda cargar
 		include(self::VIEWS_PATH . $template . "." . self::EXTENSION_TEMPLATES);
+		//Se guarda todo el contenido del buffer en una variable
 		$content = ob_get_contents();
+		//Y se limpia el buffer
 		ob_end_clean();
+		//Obtengo la ruta donde cargar todos los archivos del cliente web
 		$asset="/localhost".$_SERVER['PHP_SELF']."/../../asset/";
+		//Por ultimo, cargo el marco decorativo donde va a cargarse las vistas que los controladores ejecuten a traves de la variable $content
 		include self::VIEWS_PATH."layout/layout.".self::EXTENSION_TEMPLATES;
 	}
 	
